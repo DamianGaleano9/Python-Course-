5479fcb73f7217c7a86f60610f40beb8137a0b08
Damian Galeano <dj.damian.b@hotmail.com>
2023-02-24 21:55:07 +0000

    Strip()

---
+++ main.py
@@ -0,0 +1,45 @@
+#Python String Basics
+
+""""
+starter_sentence = "The quick brown fox jumped"
+
+first_word = starter_sentence[0]
+
+print(first_word)
+
+sentence = "HELLO THERE!"
+lowercase_sentence = sentence.lower()
+print(lowercase_sentence)
+
+"""
+
+
+#In the code below, assign the variable sub_sentence to be "pie" by selecting a range from "sentence".
+
+"""sentence = "Some pie please!"
+sub_sentence = sentence[5:8]
+print(sub_sentence)
+
+"""
+
+
+"""
+Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+
+Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+
+"""
+
+my_heredoc = """
+Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+
+Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+
+""".strip()
+
+print(my_heredoc)
+
