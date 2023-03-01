bab2ac4d608f2a0b15124cca987f9941c196ffdd
Damian Galeano <dj.damian.b@hotmail.com>
2023-03-01 21:21:21 +0000

    Update Incrementing_matrix

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
+"""
+[
+    [0, 1, 2, 3, 4],
+    [1, 2, 3, 4, 5],
+    [2, 3, 4, 5, 6],
+    [3, 4, 5, 6, 7],
+    [4, 5, 6, 7, 8],
+]
+"""
+"""
+
+
+def manual_incrementing_matrix(n):
+  matrix = [[None for y in range(n)] for x in range(n)]
+
+  counter = 0
+
+  for idx, el in enumerate(matrix):
+    for nested_idx, nested_el in enumerate(el):
+      matrix[idx][nested_idx] = counter + nested_idx
+
+      counter += 1
+
+  return matrix
+
+
+print(manual_incrementing_matrix(3))
+
+"""
+
+
+
+
+
+
+
+
+
+
+
 #Python String Basics
 
 """"
@@ -39,7 +79,10 @@ Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et
 
 Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
 
-""".strip()
+.strip()
 
-print(my_heredoc)
+print(my_heredoc)"""
+
+
+
 
